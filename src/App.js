ee6ef0b07e0a9e904f4ef6f60d18340818016bf8
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-06-03 11:33:04 +0300

    Update App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import {useState} from "react";
 import {csv} from 'd3';
+import {moment} from 'moment';
 
 function App() {
 
+  const today = moment().format();
+  console.log(today);
   const [dataFiles, setDataFiles] = useState();
   
-    const handleFileSelected = async (e) => {
-      console.log(await csv(e.target.files));
+    const handleFileSelected = (e) => {
+      
       const files = Array.from(e.target.files)
       setDataFiles(files)
       //console.log("files:", files)
     }
     
     if(dataFiles){
-      console.log("files:", dataFiles)
+      console.log("files:",csv(dataFiles))
 
     }
   
@@ -21,6 +24,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <form action="/actoni_page.php">
           <label for="myfile">Select a file:</label>
           <input onChange={handleFileSelected} type="file" id="myfile" name="myfile" />
