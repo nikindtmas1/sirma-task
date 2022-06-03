5e28dbc27bfeeaf40392e1cc62aaf3b108881e61
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-06-03 14:23:02 +0300

    Update App

--- src/App.js
+++ src/App.js
@@ -52,16 +52,21 @@ function App() {
   // handle file upload
   const handleFileUpload = e => {
     const file = e.target.files[0];
+    
     const reader = new FileReader();
     reader.onload = (evt) => {
       /* Parse data */
       const bstr = evt.target.result;
       const wb = XLSX.read(bstr, { type: 'binary' });
+      
       /* Get first worksheet */
       const wsname = wb.SheetNames[0];
+     
       const ws = wb.Sheets[wsname];
+    
       /* Convert array of arrays */
       const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+      
       processData(data);
     };
     reader.readAsBinaryString(file);
